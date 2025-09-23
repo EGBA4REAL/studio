d39210c69712ff9b538691d08e92d459c1b0321a
tessona erakora <tessona4real@gmail.com>
2025-09-23 09:53:43 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp, getApp, getApps } from 'firebase/app';
+
+const firebaseConfig = {
+  projectId: 'studio-9108138070-ae764',
+  appId: '1:569612845856:web:4dfcffc1bd706f26da4d45',
+  apiKey: 'AIzaSyAxP3eNYP1BzM3vKUjuWjuSVVftpEMnSC8',
+  authDomain: 'studio-9108138070-ae764.firebaseapp.com',
+  measurementId: '',
+  messagingSenderId: '569612845856',
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export { app };
